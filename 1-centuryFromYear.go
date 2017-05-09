961a42852908a8f58497894ab8b0b57acd628991
Xercoy <corey@prak.me>
2017-05-08 23:05:41 -0700

    Added level 1 - centuryFromYear

---
+++ 1-centuryFromYear.go
@@ -0,0 +1,50 @@
+/* Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+Example
+
+For year = 1905, the output should be
+centuryFromYear(year) = 20;
+For year = 1700, the output should be
+centuryFromYear(year) = 17.
+Input/Output
+
+[time limit] 4000ms (go)
+[input] integer year
+
+A positive integer, designating the year.
+
+Guaranteed constraints:
+1 ≤ year ≤ 2005.
+
+[output] integer
+
+The number of the century the year is in. */
+
+package main
+
+func centuryFromYear(year int) int {
+	// The base value for a century is 1.
+	century := 1
+
+	// Get the value of the two rightmost digits.
+	twoDigitValue := year % 100
+
+	// Get the value of the leftmost digit which is
+	// the hundreds place digit.
+	hundredsPlaceDigit := year / 100
+
+	// If the two digit value ends in 0, the hundreds
+	// place digit already indicates the correct century.
+	if twoDigitValue == 0 {
+		century = hundredsPlaceDigit
+	}
+
+	// If the two digit value is 1 or greater than 1, the
+	// hundreds place digit indicates a number that is 1
+	// less than the century, so add 1 to it.
+	if twoDigitValue >= 1 {
+		century = hundredsPlaceDigit + 1
+	}
+
+	return century
+}
--- main_test.go
+++ main_test.go
@@ -8,6 +8,26 @@ func TestAll(t *testing.T) {
 	testAdd(t)
 }
 
+func testCenturyFromYear(t *testing.T) {
+	inputOutputMap := map[int]int{
+		1905: 20,
+		1700: 17,
+		1988: 20,
+		2000: 20,
+		2001: 21,
+		200:  2,
+		374:  4,
+	}
+
+	for input, output := range inputOutputMap {
+		actualResult := centuryFromYear(input)
+
+		if actualResult != output {
+			t.Errorf("Failure calculating century for year %d. Caclulated %d, should be %d", input, actualResult, output)
+		}
+	}
+}
+
 func testAdd(t *testing.T) {
 	if add(5, 4) != 9 {
 		t.Errorf("Add failure with two positive numbers")
